a2c96de8fa9b86b9538e38629ad14ef627f51a07
Matt Collinge <mattycov@googlemail.com>
2024-04-08 22:42:52 +0100

    fmt and gitignore for nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "slstatus flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs
+          {
+            inherit system;
+          };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              clang-tools
+              clang
+              bear
+              cmake
+              xorg.libX11
+              xorg.libXft
+              xorg.libXinerama
+            ];
+        };
+      }
+    );
+}
+
